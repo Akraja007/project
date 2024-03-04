70adc0ee206132cb4d6781265f319696639e2395
Raja <98885373+Akraja007@users.noreply.github.com>
2024-03-04 09:30:48 +0530

    fixed all

--- OBJECT.gd
+++ OBJECT.gd
@@ -3,3 +3,6 @@ class_name OBJ
 @export var texture :Texture2D
 @export var coordinate :Vector2i =Vector2.ZERO
 @export var id :int =0
+var draging:bool = false
+var is_placed :bool =false
+var mouse_pos :Vector2
