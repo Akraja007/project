b711ad924a4f7f3c909db955485525aa57e74b86
Raja <98885373+Akraja007@users.noreply.github.com>
2024-03-21 07:39:25 +0530

    v2.3

--- script/TileMap.gd
+++ script/TileMap.gd
@@ -1,20 +1,26 @@
 extends TileMap
 @onready var ball_scene = preload("res://scene/ball.tscn")
 @onready var spring_scene =preload("res://scene/spring.tscn")
-var mouse_pos:Vector2
+var mouse_pos:Vector2i
 var used_scene_tile_pos =[]
-#---------------do it 
+
 func _on_sloat_on_drag(data):
 	mouse_pos=local_to_map(data.mouse_pos)
 	var mouseovertile = get_cell_tile_data(0,mouse_pos)
 	var is_placable
+	
 	if mouseovertile:
 		is_placable = mouseovertile.get_custom_data_by_layer_id(0)
 	if data.id== 3:
+		
 		var spring = spring_scene.instantiate()
 		get_parent().add_child(spring)
 		spring.position = map_to_local(mouse_pos)
 		data.is_placed=true
+#		print(local_to_map(used_scene_tile_pos[0]),mouse_pos)
+		for i in used_scene_tile_pos:
+			if mouse_pos == local_to_map(i):
+				print("mouse over a scene tile")
 	else : 
 		if not is_placable:
 			set_cell(0,mouse_pos,data.id,data.coordinate)
@@ -26,6 +32,5 @@ func _on_simulation_button_down():
 	var ball = ball_scene.instantiate()
 	add_child(ball)
 	ball.position = $Marker2D.position
-	print(used_scene_tile_pos)
 	pass # Replace with function body.
 
